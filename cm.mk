6fece7366ce06bf32a2a3dd8f28ab2b4578b89b8
Matt Mower <mowerm@gmail.com>
2013-10-19 19:58:49 -0500

    m7vzw: update build prop overrides to latest vzw ota
    Change-Id: Ia6f154714c67dc012a78f871327355711c698431

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7wlv BUILD_ID=JZO54K BUILD_FINGERPRINT="htc/m7wlv/m7wlv:4.2.2/JDQ39/200197.1:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.8 CL221778 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_ID=JDQ39 BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
 
 PRODUCT_NAME := cm_m7vzw
 PRODUCT_DEVICE := m7vzw
