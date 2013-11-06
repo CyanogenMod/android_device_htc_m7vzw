f9fd9924419de7c194a43805a66957ca3e0861f7
Matt Mower <mowerm@gmail.com>
2013-11-06 16:43:46 -0600

    m7vzw: access vzw apps in Play store
    Change-Id: I1603fc286cc4241c73a7e020363a117da8350972

--- cm.mk
+++ cm.mk
@@ -4,7 +4,13 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_ID=JDQ39 BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=HTCOneVZW \
+    BUILD_ID=JDQ39 \
+    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := cm_m7vzw
 PRODUCT_DEVICE := m7vzw
