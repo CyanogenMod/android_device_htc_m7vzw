5961a04a51dc479337c35c755c8abae5e89ca4fb
David Hays <dhays90@gmail.com>
2013-12-17 10:27:34 -0600

    m7vzw: update 4.3 fingerprint
    Change-Id: Ic37841669a2360fa98aff6fb38d2188d8c713a30

--- cm.mk
+++ cm.mk
@@ -6,9 +6,9 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=HTCOneVZW \
-    BUILD_ID=JDQ39 \
-    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" \
-    PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+    BUILD_ID=JSS15J \
+    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.3/JSS15J/278010.1:user/release-keys" \
+    PRIVATE_BUILD_DESC="2.10.605.1 CL278010 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
