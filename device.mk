336cd4b2a4b6610a7a52f1ef61b9267b507b90ec
David Gasaway <ponsasinorem@gmail.com>
2014-01-04 23:43:11 -1000

    m7vzw: remove calls to nonexistent overlay
    Change-Id: I73613bd00e408edbb04d98fce7f9e385bb1d59fa

--- device.mk
+++ device.mk
@@ -20,8 +20,5 @@ $(call inherit-product-if-exists, vendor/htc/m7vzw/m7vzw-vendor.mk)
 # overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/m7vzw/overlay
 
-# common overlays
-DEVICE_PACKAGE_OVERLAYS += device/htc/m7-common/overlay-cdma
-
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
