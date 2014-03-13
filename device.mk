4b4da71b1f828dca633c53bdd30318d01b4196d2
Matt Mower <mowerm@gmail.com>
2014-03-13 10:26:30 -0500

    m7vzw: remove unneeded overlay definition
    Change-Id: Ief80e861108d494c222cb4e3c8dd13cf3c3a26bd

--- device.mk
+++ device.mk
@@ -17,8 +17,5 @@
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/m7vzw/m7vzw-vendor.mk)
 
-# overlays
-DEVICE_PACKAGE_OVERLAYS += device/htc/m7vzw/overlay
-
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
