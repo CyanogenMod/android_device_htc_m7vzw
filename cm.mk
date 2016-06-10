0edcceac8df4091cea13aaa0e521fcd2f1401b0d
Matt Mower <mowerm@gmail.com>
2016-06-10 14:08:18 -0500

    m7vzw: Clean up makefile inheritance/ordering
    * Clean up cm.mk, device.mk, and full_m7vzw.mk
    * Add license header to cm.mk
    
    Change-Id: I6ab38beaa7c632b2866745aaf07cd159fc12dcb2

--- cm.mk
+++ cm.mk
@@ -1,16 +1,34 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 $(call inherit-product, device/htc/m7vzw/full_m7vzw.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=HTCOneVZW \
-    BUILD_ID=LRX22G \
-    BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" \
-    PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys"
+# Inherit CM full phone configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Device naming
 PRODUCT_NAME := cm_m7vzw
-PRODUCT_DEVICE := m7vzw
+
+# Override build props
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/HTCOneVZW/m7wlv:5.0.2/LRX22G/495599.6:user/release-keys" \
+    BUILD_ID=LRX22G \
+    PRIVATE_BUILD_DESC="6.22.605.6 CL495599 release-keys" \
+    PRODUCT_NAME=HTCOneVZW
+
+# Override ro.com.google.clientidbase
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
